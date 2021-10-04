4b181bd3306d2ef78aeafe0b80c49559b77d5672
Burekinjo <91623771+Burekinjo@users.noreply.github.com>
2021-10-04 13:38:23 +0200

    Add files via upload

---
+++ kaleida.js
@@ -0,0 +1,42 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+  }
+  
+  function draw() {
+    background(0,15);
+    rectMode(CENTER);
+  
+    translate(width/2, height/2);
+    rotate(QUARTER_PI);
+  
+    stroke (249,168,25,255);
+    fill(255, 0);
+  
+    rotate(radians(frameCount*125));
+  
+    rect(0, 0, 100, 800); 
+
+    stroke (101,209,63,255);
+    fill(255, 0);
+  
+    rotate(radians(frameCount*100));
+  
+
+    rect(0, 0, 100, 800); 
+
+    stroke (228,203,181,255);
+    fill(255, 0);
+  
+    rotate(radians(frameCount*150));
+  
+    rect(0, 0, 100, 800); 
+
+    stroke (170,209,126,255);
+    fill(255, 0);
+  
+    rotate(radians(frameCount*170));
+  
+    rect(0, 0, 100, 800);
+
+    
+  }
