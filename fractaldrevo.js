6bc8f86ff3b2aa7b5fd3beeb9e19cc6cca79d852
Burekinjo <91623771+Burekinjo@users.noreply.github.com>
2021-10-04 15:26:25 +0200

    Add files via upload

---
+++ fractaldrevo.js
@@ -0,0 +1,32 @@
+let a = 200;
+
+function setup(){
+    createCanvas(800,800);
+    background(0);
+    frameRate(20);
+}
+
+function draw(){
+    translate(400,800);
+    stroke(255);
+    strokeWeight(3);
+    draw2(a);
+}
+
+function draw2(lenght){
+
+    line(0,0,0,-lenght);
+    translate(0,-lenght);
+    if(lenght>4)
+    {
+    push();
+    rotate(QUARTER_PI*1.2);
+    draw2(lenght * 0.7);
+    pop();
+    push();
+    rotate(-(QUARTER_PI*1.2));
+    draw2(lenght * 0.7);
+    pop();
+    }
+    
+}
